653f64dfa01cf9ebb9a58a2aca9f8672374ccef8
Leviinson <votosi197@gmail.com>
2024-10-13 20:03:36 +0200

    fix: user authentication backend wasn't expecting correct arguments, superclas changed

--- accounts/backends.py
+++ accounts/backends.py
@@ -1,26 +1,22 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.backends import BaseBackend
-from django.contrib.auth.hashers import check_password
+from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
 
 
-class UserAuthenticationBackend(BaseBackend):
-    def authenticate(
-        self, request, username=None, password=None, phone_number=None, **kwargs
-    ):
-        User = get_user_model()
-        try:
-            match username, phone_number:
-                case _, None:
-                    user = User.objects.get(username=username)
-                case None, _:
-                    user = User.objects.get(phone_number=phone_number)
-                case _:
-                    return None
+class UserAuthenticationBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        UserModel = get_user_model()
+        if username is None:
+            username = kwargs.get(UserModel.USERNAME_FIELD)
+        if username is None or password is None:
+            return
 
-            if check_password(password, user.password):
+        user_query = UserModel.objects.filter(
+            Q(email=username) | Q(phone_number=username)
+        ).all()
+
+        if user_query.exists() and len(user_query) < 2:
+            user = user_query.first()
+            if user.check_password(password) and self.user_can_authenticate(user):
                 return user
-        except User.DoesNotExist:
-            return None
-        except User.MultipleObjectsReturned:
-            return None
-        return super().authenticate(request, username, password, phone_number, **kwargs)
+        return None
