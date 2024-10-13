c5b0153d901bbe2705b6846cc1a0ae264b9c1bf0
Leviinson <votosi197@gmail.com>
2024-10-13 20:03:08 +0200

    feat: user admin model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "phone_number",
+        "first_name",
+        "last_name",
+        "is_active",
+        "is_staff",
+        "date_joined",
+    )
+    list_filter = ("is_active", "is_staff", "date_joined")
+    search_fields = ("email", "phone_number", "first_name", "last_name")
+    ordering = ("-date_joined",)
+
+    fieldsets = (
+        (
+            "Личная информация",
+            {
+                "fields": ("first_name", "last_name", "phone_number", "email"),
+                "classes": [
+                    "collapse in",
+                ],
+            },
+        ),
+        (_("Важные даты"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    search_help_text = _(
+        "Поиск по адресу электронной почты, номеру телефона, имени или фамилии."
+    )
