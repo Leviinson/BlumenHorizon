4b6a09a0463b45d3e197291d804e47e68a61393c
Leviinson <votosi197@gmail.com>
2024-10-17 21:21:03 +0200

    feat(urls): signin view added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
 from django.urls import path
 
 from .views.me import UserDetailView
@@ -9,4 +10,11 @@ app_name = "accounts"
 urlpatterns = [
     path("", login_required(UserDetailView.as_view()), name="me"),
     path("signup/", UserSignUpView.as_view(), name="signup"),
+    path(
+        "signin/",
+        LoginView.as_view(
+            template_name="accounts/authorization/sign_in.html",
+        ),
+        name="signin",
+    ),
 ]
