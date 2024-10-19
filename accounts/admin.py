2efc4dc96456a8472a979ec19fd533622fa368b8
Leviinson <votosi197@gmail.com>
2024-10-19 17:40:13 +0200

    feat(admin): fieldsets updated

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 
+from .forms.user_form import UserForm
 from .models import User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    form = UserForm
     list_display = (
         "email",
-        "phone_number",
+        "phonenumber",
         "first_name",
         "last_name",
         "is_active",
@@ -16,20 +18,37 @@ class UserAdmin(admin.ModelAdmin):
         "date_joined",
     )
     list_filter = ("is_active", "is_staff", "date_joined")
-    search_fields = ("email", "phone_number", "first_name", "last_name")
+    search_fields = ("email", "phonenumber", "first_name", "last_name")
     ordering = ("-date_joined",)
 
     fieldsets = (
         (
-            "Личная информация",
+            "Personal info",
             {
-                "fields": ("first_name", "last_name", "phone_number", "email"),
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "phonenumber",
+                    "email",
+                    "is_active",
+                ),
                 "classes": [
-                    "collapse in",
+                    "wide",
                 ],
             },
         ),
-        (_("Важные даты"), {"fields": ("last_login", "date_joined")}),
+        (
+            "Important dates",
+            {
+                "fields": (
+                    "last_login",
+                    "date_joined",
+                ),
+                "classes": [
+                    "wide",
+                ],
+            },
+        ),
     )
 
     search_help_text = _(
