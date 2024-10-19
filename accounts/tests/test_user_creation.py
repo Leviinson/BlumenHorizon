730647679494f4def18baaf0772a39f2f4329b68
Leviinson <votosi197@gmail.com>
2024-10-19 17:47:43 +0200

    feat(test): test for checking of user creation with dupliated email/phonenumber created

--- accounts/tests/test_user_creation.py
+++ accounts/tests/test_user_creation.py
@@ -1,74 +1,106 @@
 from dataclasses import asdict, dataclass
 
 from django.contrib.auth import get_user_model
-from django.test import TestCase
-from django.urls import reverse_lazy
+
+from core.tests.base import BaseTestCase
 
 
-class UserCreationTest(TestCase):
+@dataclass
+class UserCredentials:
+    email: str
+    password1: str
+    password2: str
+    first_name: str
+    last_name: str
+    phonenumber_0: str = "DE"
+    phonenumber_1: str = "15234815621"
 
-    @dataclass
-    class UserCredentials:
-        email: str = "melnykov.vitalii197@gmail.com"
-        password1: str = "fhjeuio**^321"
-        password2: str = "fhjeuio**^321"
-        first_name: str = "Vitalii"
-        last_name: str = "Melnykov"
 
-    def setUp(self):
-        self.signup_url = reverse_lazy("accounts:signup")
-        self.signin_url = reverse_lazy("accounts:signin")
-        self.signup_data = self.UserCredentials()
-        return super().setUp()
-    
+class UserCreationTest(BaseTestCase):
+
+    def create_user(self, signup_data: UserCredentials):
+        return self.client.post(self.signup_url, asdict(signup_data))
+
+    def assert_form_error(self, form, field, message):
+        self.assertFalse(form.is_valid(), f"Form should not be valid for {field}")
+        self.assertIn(field, form.errors)
+        self.assertEqual(form.errors[field][0], message)
+
     def test_getting_reverse_url(self):
         str(self.signin_url)
 
     def test_user_creation(self):
+        signup_data = UserCredentials(
+            email="melnykov.vitalii197@gmail.com",
+            password1="fhjeuio**^321",
+            password2="fhjeuio**^321",
+            first_name="Vitalii",
+            last_name="Melnykov",
+        )
+        response = self.create_user(signup_data)
 
-        response = self.client.post(self.signup_url, asdict(self.signup_data))
-        if response.status_code == 200 and response.content["form"].errors:
-            self.fail(
-                f"Form errors: {response.context["form"].errors}.\nUser wasn't created."
-            )
         self.assertRedirects(response, self.signin_url)
 
-        user_filtered = get_user_model().objects.filter(email=self.signup_data.email)
-        self.assertTrue(
-            user_filtered.exists(),
-            f"The controller of user creation returned 201 code"
-            f" but record in DB wasn't created.",
-        )
+        user_filtered = get_user_model().objects.filter(email=signup_data.email)
+        self.assertTrue(user_filtered.exists(), "User record wasn't created.")
+
         user = user_filtered.first()
-        self.assertEqual(
-            user.email,
-            self.signup_data.email,
-            "Username from request and in DB is different",
+        self.assertEqual(user.email, signup_data.email)
+        self.assertEqual(user.phonenumber, "+49" + signup_data.phonenumber_1)
+        self.assertEqual(user.first_name, signup_data.first_name)
+        self.assertEqual(user.last_name, signup_data.last_name)
+        self.assertTrue(user.check_password(signup_data.password1))
+        self.assertFalse(user.is_active)
+
+    def test_user_creation_with_duplicated_phone_number(self):
+        signup_data_1 = UserCredentials(
+            email="melnykov.vitalii197@gmail.com",
+            password1="fhjeuio**^321",
+            password2="fhjeuio**^321",
+            first_name="Vitalii",
+            last_name="Melnykov",
         )
-        self.assertEqual(
-            user.first_name,
-            self.signup_data.first_name,
-            "User first name from request and in DB is different",
+        self.create_user(signup_data_1)
+
+        signup_data_2 = UserCredentials(
+            email="secret197@gmail.com",
+            password1="fhjeuio**^321",
+            password2="fhjeuio**^321",
+            first_name="Vitalii",
+            last_name="Melnykov",
         )
-        self.assertEqual(
-            user.last_name,
-            self.signup_data.last_name,
-            "User last name from request and in DB is different",
+        response = self.create_user(signup_data_2)
+
+        self.assertEqual(response.status_code, 200)
+        self.assert_form_error(
+            response.context["form"],
+            "phonenumber",
+            "Пользователь с этим номером телефона уже существует.",
         )
-        self.assertTrue(
-            user.check_password(self.signup_data.password1),
-            "Password from request and in DB is different",
+
+    def test_user_creation_with_duplicated_email(self):
+        signup_data_1 = UserCredentials(
+            email="melnykov.vitalii197@gmail.com",
+            password1="fhjeuio**^321",
+            password2="fhjeuio**^321",
+            first_name="Vitalii",
+            last_name="Melnykov",
         )
-        self.assertTrue(
-            user.is_authenticated,
-            "User wasn't authenticated",
+        self.create_user(signup_data_1)
+
+        signup_data_2 = UserCredentials(
+            email="melnykov.vitalii197@gmail.com",
+            password1="fhjeuio**^321",
+            password2="fhjeuio**^321",
+            first_name="Vitalii",
+            last_name="Melnykov",
+            phonenumber_1="15234815622",
         )
-        self.assertFalse(
-            user.is_anonymous,
-            "User was authenticated and is anonymous at the same time",
+        response = self.create_user(signup_data_2)
+
+        self.assertEqual(response.status_code, 200)
+        self.assert_form_error(
+            response.context["form"],
+            "email",
+            "Пользователь с этой электронной почтой уже существует.",
         )
-        self.assertFalse(
-            user.is_active, "User is active but email wasn't confirmed yet"
-        )
-        self.assertFalse(user.is_staff, "User was created as staff")
-        self.assertFalse(user.is_superuser, "User was created as is_superuser")
