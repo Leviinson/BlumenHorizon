46e1a7082cbab8b073ae07dbbc414051324e6631
Leviinson <votosi197@gmail.com>
2024-10-19 17:48:26 +0200

    feat(services): decorator for db queries inspecting in function created

---
+++ core/services/decorators/db/db_queries.py
@@ -0,0 +1,28 @@
+from functools import wraps
+from pprint import pprint
+from typing import Callable
+
+from django.db import connection, reset_queries
+
+
+def inspect_db_queries(function: Callable):
+    """
+    To inspect method db queries.
+    """
+
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        reset_queries()
+        result = function(*args, **kwargs)
+        print(
+            f"Function: {function.__name__}",
+            (f"Args: {args}" if args else ""),
+            (f"Kwargs: {kwargs}" if kwargs else ""),
+            sep="\n",
+            end="\n\n",
+        )
+        pprint(connection.queries)
+        print("\n\n")
+        return result
+
+    return wrapper
