4fb168fcd13667f1653945e492dde67a98bfc5ed
Leviinson <votosi197@gmail.com>
2024-10-19 17:49:41 +0200

    feat(tests): BaseTestClass created

--- core/tests/base.py
+++ core/tests/base.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse_lazy
 
 
 class BaseTestCase(TestCase):
     @classmethod
     def setUpClass(cls):
-        pass
+        cls.signup_url = reverse_lazy("accounts:signup")
+        cls.signin_url = reverse_lazy("accounts:signin")
+        super().setUpClass()
