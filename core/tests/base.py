8021d5c690f535c7253f1ea9ad877d863d1552ff
Leviinson <votosi197@gmail.com>
2024-10-20 21:21:43 +0200

    feat(tests): BaseTest class now has site data (name, domain) and test user

--- core/tests/base.py
+++ core/tests/base.py
@@ -1,9 +1,11 @@
 from dataclasses import asdict
 
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
 from django.test import TestCase
 from django.urls import reverse_lazy
+from django.contrib.sites.models import Site
 
 from .common_dataclasses.user import UserCredentials
 
@@ -14,18 +16,23 @@ class BaseTestCase(TestCase):
         cls.signup_url = reverse_lazy("accounts:signup")
         cls.signin_url = reverse_lazy("accounts:signin")
         cls.profile_url = reverse_lazy("accounts:me")
+        site = Site.objects.get(id=1)
+        site.name = settings.SITE_NAME
+        site.domain = settings.SITE_DOMAIN
+        site.save(update_fields=["name", "domain",])
 
         User = get_user_model()
-        cls.signin_data = UserCredentials(
+        cls.test_user_password = "secret123321"
+        cls.test_user_data = UserCredentials(
             email="secret@gmail.com",
             phonenumber="+4915234815623",
             first_name="Vitalii",
             last_name="Melnykov",
-            password=make_password("secret123321")
         )
-        cls.user = User.objects.create(
-            **asdict(cls.signin_data),
+        cls.user = User(
+            **asdict(cls.test_user_data),
             is_active = True
         )
-        cls.user.set_password("secret123321")
+        cls.user.set_password(cls.test_user_password)
+        cls.user.save()
         super().setUpClass()
