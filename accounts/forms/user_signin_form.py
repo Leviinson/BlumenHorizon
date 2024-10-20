35fbafd16b358b74e2defbc36dc407b97bbeba0e
Leviinson <votosi197@gmail.com>
2024-10-20 21:10:35 +0200

    feat(forms): UserSignInForm created

---
+++ accounts/forms/user_signin_form.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import gettext_lazy as _
+
+
+class UserSignInForm(AuthenticationForm):
+    remember = forms.BooleanField(required=False, initial=False)
+
+    error_messages = {
+        "invalid_login": _(
+            "Пожалуйста, введите корректную электронную почтуи пароль. Помните, что "
+            "оба поля чувствительны к регистру"
+        ),
+        "inactive": _('Почта не подтверждена. Мы отправили письмо на Вашу почту, '
+                      'проверьте папку "Входящие"/"Спам".'),
+    }
