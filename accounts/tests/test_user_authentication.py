6ac8df841c3a3b457a45db0a96e743fced7325a7
Leviinson <votosi197@gmail.com>
2024-10-20 21:18:50 +0200

    feat(tests): added test of user logging in with email/phonenumber

--- accounts/tests/test_user_authentication.py
+++ accounts/tests/test_user_authentication.py
@@ -1,11 +1,34 @@
-from dataclasses import asdict
+from dataclasses import asdict, dataclass
+from pprint import pprint
+
 from core.tests.base import BaseTestCase
-from core.tests.common_dataclasses.user import UserSignUpCredentials
 
 
 class UserAuthenticationTest(BaseTestCase):
+    @dataclass
+    class TestUserAuthenticationData:
+        username: str
+        password: str
 
-    def test_user_authentication(self):
-        response = self.client.post(self.signin_url, asdict(self.signin_data))
-        self.assertTrue(response.context["user"].is_authenticated, "User is not authenticated")
+    def test_user_authentication_with_email(self):
+        self.__test_user_authenticatinon(
+            self.TestUserAuthenticationData(
+                username=self.test_user_data.email, password=self.test_user_password
+            )
+        )
+
+    def test_user_authentication_with_phonenumber(self):
+        self.__test_user_authenticatinon(
+            self.TestUserAuthenticationData(
+                username=self.test_user_data.phonenumber,
+                password=self.test_user_password,
+            )
+        )
+
+    def __test_user_authenticatinon(self, signin_data: dict[str, str]):
+        response = self.client.post(self.signin_url, asdict(signin_data))
         self.assertRedirects(response, self.profile_url)
+        self.assertTrue(
+            response.wsgi_request.user.is_authenticated,
+            "User is not authenticated",
+        )
