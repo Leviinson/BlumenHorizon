7623bcf87f7ef94f5047319b5677f0dd8d86e947
Leviinson <votosi197@gmail.com>
2024-10-20 21:19:19 +0200

    feat(urls): added url for email activation

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 from .views.me import UserDetailView
 from .views.signin import UserLoginView
-from .views.signup import UserSignUpView
+from .views.signup import UserSignUpView, activate_user_account
 
 app_name = "accounts"
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path("", login_required(UserDetailView.as_view()), name="me"),
     path("signup/", UserSignUpView.as_view(), name="signup"),
     path("signin/", UserLoginView.as_view(), name="signin"),
+    path("activate/<uidb64>/<token>/", activate_user_account, name="activate"),
 ]
