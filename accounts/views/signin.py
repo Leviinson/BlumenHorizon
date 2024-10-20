f6b616ea9814881c0f81e8de8efc7648b1c427a3
Leviinson <votosi197@gmail.com>
2024-10-20 21:19:44 +0200

    feat(views): UserLoginView added

--- accounts/views/signin.py
+++ accounts/views/signin.py
@@ -1,15 +1,30 @@
+from pprint import pprint
 from django.contrib.auth.views import LoginView
 from django.utils.translation import gettext_lazy as _
 from django.views.generic.base import ContextMixin
 
-from core.services.mixins.views import CommonContextMixin, NotAuthenticatedMixin
+from core.services.mixins.views import CommonContextMixin
+from ..forms import UserSignInForm
 
 
 class UserLoginView(
-    NotAuthenticatedMixin,
     CommonContextMixin,
     LoginView,
     ContextMixin,
 ):
     extra_context = {"title": _("Вход")}
     template_name = "accounts/authorization/sign_in.html"
+    redirect_authenticated_user = True
+    authentication_form = UserSignInForm
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        if form.cleaned_data.get('remember'):
+            self.request.session.set_expiry(7 * 24 * 60 * 60)
+        else:
+            self.request.session.set_expiry(0)
+        return response
+    
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        return response
