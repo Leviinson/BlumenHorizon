21f2dce0aa9e2715ad88b8d6d0baa5003533b801
Leviinson <votosi197@gmail.com>
2024-10-22 10:39:36 +0200

    feat(models): TimeStampModelMixin renamed to TimeStampAdbstractModel

--- core/base_models.py
+++ core/base_models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
-class TimeStampModelMixin(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class TimeStampAdbstractModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Дата создания"))
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_("Дата обновления"))
 
     class Meta:
         abstract = True
