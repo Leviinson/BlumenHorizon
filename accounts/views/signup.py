2184b65c478b6d0588b33240f73082ae75a94f78
Leviinson <votosi197@gmail.com>
2024-10-22 23:35:49 +0200

    fix(translations): variable inserting into string fixed

--- accounts/views/signup.py
+++ accounts/views/signup.py
@@ -52,8 +52,7 @@ class UserSignUpView(
         user: Type[AbstractBaseUser],
         to_email: str,
     ):
-        site_name = get_current_site(request).name
-        mail_subject = _(f"{site_name} | Подтвердите Ваш Email")
+        mail_subject = _("{site_name} | Подтвердите Ваш Email").format(site_name=get_current_site(request).name)
 
         message = render_to_string(
             "accounts/email_confirmation/email_body.html",
