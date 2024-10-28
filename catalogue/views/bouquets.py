d0cdefb1e9ba427e522d3bec34b072622fa688f5
Leviinson <votosi197@gmail.com>
2024-10-28 13:35:14 +0100

    feat(views): querysets for detail view and list view added

--- catalogue/views/bouquets.py
+++ catalogue/views/bouquets.py
@@ -1,41 +1,78 @@
+from django.db.models import OuterRef, Subquery
 from django.utils.translation import gettext_lazy as _
 from django.views.generic.base import ContextMixin, TemplateResponseMixin
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django_filters.views import FilterView
 
-
 from core.services.mixins.views import CommonContextMixin
 
-from ..models import Bouquet
-from ..services.views import ListViewMixin
+from ..filters import BouquetFilter
+from ..models import Bouquet, BouquetImage
+from ..services.views import DetailViewMixin, ListViewMixin
 
 
 class BouquetView(
+    DetailViewMixin,
     CommonContextMixin,
     DetailView,
     TemplateResponseMixin,
     ContextMixin,
 ):
     model = Bouquet
-    queryset = Bouquet.objects.filter(is_active=True).order_by("name")
+    queryset = (
+        Bouquet.objects.filter(is_active=True)
+        .prefetch_related("images", "colors", "flowers")
+        .select_related(
+            "subcategory",
+            "subcategory__category",
+        )
+        .only(
+            "name",
+            "price",
+            "amount_of_flowers",
+            "description",
+            "specs",
+            "images",
+            "discount",
+            "subcategory__slug",
+            "subcategory__category__slug",
+            "colors__name",
+            "colors__hex_code",
+            "flowers__name",
+        )
+    )
     context_object_name = "product"
     slug_url_kwarg = "bouquet_slug"
     template_name = "products/bouquets/bouquet_detail.html"
 
-    def get_context_data(self, *args, **kwargs):
-        kwargs["title"] = self.object.name
-        return super().get_context_data(*args, **kwargs)
-    
 
 class BouquetListView(
     ListViewMixin,
     CommonContextMixin,
-    ListView,
+    FilterView,
     TemplateResponseMixin,
     ContextMixin,
 ):
     model = Bouquet
-    queryset = Bouquet.objects.filter(is_active=True).order_by("name")
+    queryset = (
+        Bouquet.objects
+        .select_related(
+            "subcategory__category",
+        )
+        .only(
+            "slug",
+            "name",
+            "price",
+            "images",
+            "discount",
+            "subcategory__slug",
+            "subcategory__category__slug",
+        )
+    )
     context_object_name = "products"
     template_name = "products/bouquets/bouquet_list.html"
+    filterset_class = BouquetFilter
+    extra_context = {"title": _("Каталог букетов")}
+    image_model = BouquetImage
+    image_model_related_name = "bouquet"
