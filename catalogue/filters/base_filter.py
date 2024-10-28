b41f691bdc4910cf80db11df7fb76330d8f5ae20
Leviinson <votosi197@gmail.com>
2024-10-28 13:54:36 +0100

    feat(filters): with_discount filter removed

--- catalogue/filters/base_filter.py
+++ catalogue/filters/base_filter.py
@@ -5,12 +5,6 @@ from django.utils.translation import gettext_lazy as _
 from django_filters import BooleanFilter, FilterSet, NumberFilter
 
 
-class EnabledBooleanFilter(BooleanFilter):
-    __field_class = forms.NullBooleanField
-    __field_class.initial = True
-    field_class = __field_class
-
-
 class BaseFilter(FilterSet):
     aggregate_fields = []
 
@@ -23,13 +17,6 @@ class BaseFilter(FilterSet):
         field_name="price",
         lookup_expr="lte",
         label=_("Максимальная цена"),
-    )
-    with_discount = EnabledBooleanFilter(
-        label=_("Со скидкой"),
-        field_name="discount",
-        widget=forms.CheckboxInput(attrs={"class": "form-check-input checkbox-dark"}),
-        method="filter_with_discount",
-        initial=True,
     )
 
     def __init__(self, *args, **kwargs):
@@ -57,7 +44,6 @@ class BaseFilter(FilterSet):
     class Meta:
         fields = [
             "price",
-            "discount",
         ]
 
     def get_aggregate_limits(self):
@@ -77,5 +63,3 @@ class BaseFilter(FilterSet):
 
         return self.Meta.model.objects.aggregate(**aggregation)
 
-    def filter_with_discount(self, queryset, name, value):
-        return queryset.filter(discount__gt=0) if value else queryset.filter(discount=0)
