8e6c0adfc4ad437b25b90cc8cbd5f9c068e3953c
Leviinson <votosi197@gmail.com>
2024-10-28 13:31:41 +0100

    feat(ranges): noUiSlider for ranges of bouquet size and amount of flowers created

--- catalogue/templates/products/scripts/noUiSlider.js
+++ catalogue/templates/products/scripts/noUiSlider.js
@@ -1,32 +1,45 @@
-var slider = document.getElementById('priceSlider');
-var minInput = document.getElementById('price__gte');
-var maxInput = document.getElementById('price__lte');
-var maxRange = parseInt(maxInput.getAttribute('max'));
+function createSlider(sliderId, minInputId, maxInputId, step = 10) {
+    var slider = document.getElementById(sliderId);
+    var minInput = document.getElementById(minInputId);
+    var maxInput = document.getElementById(maxInputId);
+    var minRange = parseInt(minInput.getAttribute('min'));
+    var maxRange = parseInt(maxInput.getAttribute('max'));
 
-noUiSlider.create(slider, {
-    start: [minInput.value, maxInput.value],
-    connect: true,
-    range: {
-        'min': 0,
-        'max': maxRange
-    },
-    step: 0.1
-});
+    noUiSlider.create(slider, {
+        start: [minInput.value, maxInput.value],
+        connect: true,
+        range: {
+            'min': minRange,
+            'max': maxRange
+        },
+        step: step
+    });
 
+    slider.noUiSlider.on('update', function (values, handle) {
+        if (handle === 0) {
+            minInput.value = Math.round(values[0]);
+        } else {
+            maxInput.value = Math.round(values[1]);
+        }
+    });
 
-slider.noUiSlider.on('update', function (values, handle) {
-    if (handle === 0) {
-        minInput.value = Math.round(values[0]);
-    } else {
-        maxInput.value = Math.round(values[1]);
+    minInput.addEventListener('input', function () {
+        slider.noUiSlider.set([this.value, null]);
+    });
+
+    maxInput.addEventListener('input', function () {
+        slider.noUiSlider.set([null, this.value]);
+    });
+}
+
+const sliders = [
+    { sliderId: 'priceSlider', minInputId: 'min_price_input', maxInputId: 'max_price_input' },
+    { sliderId: 'sizeSlider', minInputId: 'min_size_input', maxInputId: 'max_size_input' },
+    { sliderId: 'amountOfFlowersSlider', minInputId: 'min_amount_of_flowers_input', maxInputId: 'max_amount_of_flowers_input' },
+];
+
+sliders.forEach(({ sliderId, minInputId, maxInputId }) => {
+    if (document.getElementById(sliderId)) {
+        createSlider(sliderId, minInputId, maxInputId);
     }
-});
-
-
-minInput.addEventListener('input', function () {
-    slider.noUiSlider.set([this.value, null]);
-});
-
-maxInput.addEventListener('input', function () {
-    slider.noUiSlider.set([null, this.value]);
 });
