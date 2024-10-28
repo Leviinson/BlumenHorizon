a4e1b994d0e96dc43961eb9f3370f705b5e91100
Leviinson <votosi197@gmail.com>
2024-10-28 13:36:25 +0100

    feat(views): queryset for products list view added, attributes for ListViewMixin specified

--- catalogue/views/products.py
+++ catalogue/views/products.py
@@ -1,15 +1,13 @@
-from django import forms
-from django.db.models import Max, Min
+from django.utils.translation import gettext_lazy as _
 from django.views.generic.base import ContextMixin, TemplateResponseMixin
 from django.views.generic.detail import DetailView
-from django_filters import BooleanFilter, FilterSet
 from django_filters.views import FilterView
-from django.utils.translation import gettext_lazy as _
+
 from core.services.mixins.views import CommonContextMixin
 
+from ..filters import ProductFilter
 from ..models import Product, ProductImage
-from ..services.views import ListViewMixin, DetailViewMixin
-from django.db.models import OuterRef, Subquery
+from ..services.views import DetailViewMixin, ListViewMixin
 
 
 class ProductView(
@@ -22,7 +20,6 @@ class ProductView(
     model = Product
     queryset = (
         Product.objects.filter(is_active=True)
-        .order_by("name")
         .prefetch_related("images")
         .select_related(
             "subcategory",
@@ -41,63 +38,12 @@ class ProductView(
     )
     context_object_name = "product"
     slug_url_kwarg = "product_slug"
-    template_name = "products/products/product_detail.html"
+    template_name = "products/base_detail.html"
 
     def get_context_data(self, *args, **kwargs):
-        kwargs["title"] = self.object.name
-        kwargs["images_url"] = (image.image.url for image in self.object.images.all())
-        return super().get_context_data(*args, **kwargs)
-
-
-class ProductFilter(FilterSet):
-    with_discount = BooleanFilter(
-        label="Со скидкой?",
-        field_name="discount",
-        method="filter_with_discount",
-        widget=forms.CheckboxInput(attrs={"class": "form-check-input checkbox-dark"}),
-    )
-
-    class Meta:
-        model = Product
-        fields = {
-            "price": ["gte", "lte"],
-        }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        price_limits = Product.objects.aggregate(
-            min_price=Min("price"), max_price=Max("price")
-        )
-        self.form.fields["price__gte"].widget.attrs.update(
-            {
-                "class": "form-control",
-                "id": "price__gte",
-                "min": 0,
-                "max": (
-                    price_limits["max_price"]
-                    if price_limits["max_price"] is not None
-                    else 0
-                ),
-            }
-        )
-        self.form.fields["price__lte"].widget.attrs.update(
-            {
-                "class": "form-control",
-                "id": "price__lte",
-                "min": 0,
-                "max": (
-                    price_limits["max_price"]
-                    if price_limits["max_price"] is not None
-                    else 0
-                ),
-            }
-        )
-
-    def filter_with_discount(self, queryset, name, value):
-        if value:
-            queryset = queryset.filter(discount__gt=0)
-            return queryset
-        return queryset
+        context = super().get_context_data(*args, **kwargs)
+        context["title"] = self.object.name
+        return context
 
 
 class ProductListView(
@@ -108,19 +54,25 @@ class ProductListView(
     ContextMixin,
 ):
     model = Product
-    context_object_name = "products"
-    template_name = "products/products/product_list.html"
-    filterset_class = ProductFilter
-    ordering = ("price",)
-    extra_context = {"title": _("Каталог букетов")}
-
-    def get_queryset(self):
-        first_image_subquery = ProductImage.objects.filter(
-            product=OuterRef("pk")
-        ).order_by("id")[:1]
-
-        return (
-            Product.objects.filter(is_active=True)
-            .order_by("name")
-            .annotate(first_image=Subquery(first_image_subquery.values("image")[:1]))
+    queryset = (
+        Product.objects.filter(is_active=True)
+        .select_related(
+            "subcategory__category",
         )
+        .only(
+            "slug",
+            "name",
+            "price",
+            "images",
+            "discount",
+            "subcategory__slug",
+            "subcategory__category__slug",
+        )
+    )
+    ordering = ("name",)
+    context_object_name = "products"
+    template_name = "products/base_list.html"
+    filterset_class = ProductFilter
+    extra_context = {"title": _("Каталог продуктов")}
+    image_model = ProductImage
+    image_model_related_name = "product"
