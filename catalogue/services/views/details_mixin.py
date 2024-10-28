db8aa8559e68bd213897447b40b5b9a75163e609
Leviinson <votosi197@gmail.com>
2024-10-28 13:30:01 +0100

    feat(mixins): common code from catalogue views moved to common mixins

--- catalogue/services/views/details_mixin.py
+++ catalogue/services/views/details_mixin.py
@@ -4,6 +4,10 @@ from typing import Any
 class DetailViewMixin:
     def get_context_data(self, *args, **kwargs) -> dict[str, Any]:
         context = super().get_context_data(*args, **kwargs)
-        context["category_slug_breadcrumbs"] = self.object.subcategory.category.slug
-        context["subcategory_slug_breadcrumbs"] = self.object.subcategory.slug
+        kwargs["title"] = self.object.name
+        kwargs["images_url"] = (image.image.url for image in self.object.images.all())
         return context
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(subcategory__slug=self.kwargs["subcategory_slug"])
--- catalogue/services/views/list_mixin.py
+++ catalogue/services/views/list_mixin.py
@@ -1,10 +1,20 @@
-from typing import Any
+from django.db.models import OuterRef, Subquery
 
 
 class ListViewMixin:
     allow_empty = True
     paginate_by = 9
+    image_model = None
+    image_model_related_name = None
 
-    def get_context_data(self, *args, **kwargs) -> dict[str, Any]:
-        context = super().get_context_data(self, *args, **kwargs)
-        return context
+    def get_queryset(self):
+        qs = super().get_queryset()
+        first_image_subquery = self.image_model.objects.filter(
+            **{
+                self.image_model_related_name: OuterRef("pk"),
+            }
+        ).order_by("id")[:1]
+
+        return qs.filter(is_active=True).annotate(
+            first_image_uri=Subquery(first_image_subquery.values("image")[:1]),
+        ).order_by("name")
