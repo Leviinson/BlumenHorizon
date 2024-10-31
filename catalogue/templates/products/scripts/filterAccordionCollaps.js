d1f0cdd71631823767c694babe6a6c7c808da0ce
Leviinson <votosi197@gmail.com>
2024-10-31 12:47:43 +0100

    feat(filters): filters are hidden on start

--- catalogue/templates/products/scripts/filterAccordionCollaps.js
+++ catalogue/templates/products/scripts/filterAccordionCollaps.js
@@ -2,15 +2,23 @@
 
 const filtersButton = document.getElementById('filtersButton');
 const card = document.getElementById('filtersCard');
+const filterAccordion = document.getElementById('filterAccordion');
 
 let flag = 0 // to check if button was clicked
 function updateBorder() {
-    if (flag % 2 == 0 ) {
-        card.style.border = '1px solid #ccc';
+    const mediaQuery = window.matchMedia('(max-width: 767px)');
+
+    if (mediaQuery.matches) {
+        if (flag % 2 == 1 ) {
+            card.style.border = '1px solid #ccc';
+        } else {
+            card.classList.add("show");
+            card.style.border = 'none';
+        }
+        flag += 1
     } else {
-        card.style.border = 'none';
+        filterAccordion.classList.add("show");
     }
-    flag += 1
 }
 updateBorder();
 filtersButton.addEventListener('click', updateBorder);
