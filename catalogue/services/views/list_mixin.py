5c4fafa0182e99e8e6df80c1cfe297d39867abeb
Leviinson <votosi197@gmail.com>
2024-10-31 14:35:46 +0100

    feat(mixins): products list sorting added

--- catalogue/services/views/list_mixin.py
+++ catalogue/services/views/list_mixin.py
@@ -1,4 +1,5 @@
 from django.db.models import OuterRef, Subquery
+from django.utils.translation import gettext_lazy as _
 
 
 class ListViewMixin:
@@ -6,6 +7,13 @@ class ListViewMixin:
     paginate_by = 8
     image_model = None
     image_model_related_name = None
+
+    SORT_OPTIONS = [
+        {"name": _("Цена по убыванию"), "value": "pd"},
+        {"name": _("Цена по возрастанию"), "value": "pi"},
+        {"name": _("По алфавиту"), "value": "alph"},
+        {"name": _("Со скидкой"), "value": "disc"},
+    ]
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -15,6 +23,30 @@ class ListViewMixin:
             }
         ).order_by("id")[:1]
 
-        return qs.filter(is_active=True, subcategory__is_active=True, subcategory__category__is_active=True).annotate(
-            first_image_uri=Subquery(first_image_subquery.values("image")[:1]),
-        ).order_by("name")
+        sort_option = self.request.GET.get('sort', 'pd')
+        
+        match sort_option:
+            case 'pd':
+                qs = qs.order_by('-price')
+            case 'pi':
+                qs = qs.order_by('price')
+            case 'alph':
+                qs = qs.order_by('name')
+            case 'disc':
+                qs = qs.order_by('-discount')
+
+        return (
+            qs.filter(
+                is_active=True,
+                subcategory__is_active=True,
+                subcategory__category__is_active=True,
+            )
+            .annotate(
+                first_image_uri=Subquery(first_image_subquery.values("image")[:1]),
+            )
+        )
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['sort_options'] = self.SORT_OPTIONS
+        return context
