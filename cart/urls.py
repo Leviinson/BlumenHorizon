54fd1e5d363f4b924b68806dd19d5f42f6e55195
Leviinson <votosi197@gmail.com>
2024-11-01 12:49:13 +0100

    feat(urls): views name updated

--- cart/urls.py
+++ cart/urls.py
@@ -1,24 +1,36 @@
 from django.urls import path
 
 from .views import (
-    CartBouquetAdd,
-    CartBouquetRemove,
-    CartBouquetRemoveSingle,
-    CartProductAdd,
-    CartProductRemove,
-    CartProductRemoveSingle,
+    CartBouquetAddView,
+    CartBouquetRemoveSingleView,
+    CartBouquetRemoveView,
+    CartProductAddView,
+    CartProductRemoveSingleView,
+    CartProductRemoveView,
     CartView,
     cart_clear,
 )
 
 app_name = "cart"
 urlpatterns = (
-    path("bouquet/increase/", CartBouquetAdd.as_view(), name="bouquet-add"),
-    path("bouquet/remove/", CartBouquetRemove.as_view(), name="bouquet-remove"),
-    path("bouquet/decrease/", CartBouquetRemoveSingle.as_view(), name="bouquet-remove-single"),
-    path("product/increase/", CartProductAdd.as_view(), name="product-add"),
-    path("product/remove/", CartProductRemove.as_view(), name="product-remove"),
-    path("product/decrease/", CartProductRemoveSingle.as_view(), name="product-remove-single"),
+    path("bouquet/increase/", CartBouquetAddView.as_view(), name="bouquet-add"),
+    path(
+        "bouquet/remove/", CartBouquetRemoveView.as_view(), name="bouquet-remove"
+    ),
+    path(
+        "bouquet/decrease/",
+        CartBouquetRemoveSingleView.as_view(),
+        name="bouquet-remove-single",
+    ),
+    path("product/increase/", CartProductAddView.as_view(), name="product-add"),
+    path(
+        "product/remove/", CartProductRemoveView.as_view(), name="product-remove"
+    ),
+    path(
+        "product/decrease/",
+        CartProductRemoveSingleView.as_view(),
+        name="product-remove-single",
+    ),
     path("clear/", cart_clear, name="clear"),
     path("", CartView.as_view(), name="show"),
 )
