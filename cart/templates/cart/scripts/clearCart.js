df95f3c804c0fa973f39aa0983590cf39be610c3
Leviinson <votosi197@gmail.com>
2024-11-01 15:48:24 +0100

    feat(scripts): cart clear ajax request sending added

---
+++ cart/templates/cart/scripts/clearCart.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const cartClearForm = document.getElementById('cart-clear-form');
+
+    async function sendAjax(form) {
+        try {
+            const response = await fetch(form.action, {
+                method: 'POST',
+                body: new FormData(form)
+            });
+
+            const data = await response.json();
+
+            if (data.status === "success") {
+                const productsListContainer = document.getElementById("products-list-container");
+                const grandTotalElement = document.getElementById("grand-total-price");
+                const countElement = document.getElementById("total-count");
+                const productsTotalPrice = document.getElementById('products-total-price');
+                const productContainers = productsListContainer.querySelectorAll(".product-container")
+                const hrElements = productsListContainer.querySelectorAll("hr");
+                if (productsListContainer && grandTotalElement && countElement) {
+                    countElement.textContent = data.count;
+                    grandTotalElement.textContent = data.grand_total;
+                    productsTotalPrice.textContent = data.grand_total;
+                    productContainers.forEach(container => container.remove());
+                    hrElements.forEach(hr => hr.remove());
+                    checkCardBodyContent();
+                }
+                showToast(data.message, "danger");
+            } else {
+                showToast(data.message, "danger");
+            }
+        } catch (error) {
+            console.log(error);
+            showToast(gettext("Произошла ошибка. Повторите попытку позже."), "danger");
+        }
+    }
+
+    cartClearForm.addEventListener('submit', function (e) {
+            e.preventDefault();
+            sendAjax(cartClearForm);
+        });
+    });
