ffb20025cd6edb5f8e16aeac68829e0976706b9a
Leviinson <votosi197@gmail.com>
2024-11-01 15:48:04 +0100

    feat(scripts): renamed to "emptyCartHandler", buttons disabling added

--- cart/templates/cart/scripts/emptyCartHandler.js
+++ cart/templates/cart/scripts/emptyCartHandler.js
@@ -1,20 +1,26 @@
-function checkCardBodyContent() {
+function emptyCartHandler() {
     const productsListContainer = document.getElementById("products-list-container");
     const noImageContainer = document.getElementById("no-image-container");
     const checkoutButton = document.getElementById("btn-checkout");
-    console.log(document.querySelectorAll(".product-container"));
+    const cartClearButton = document.getElementById("cart-clear-button");
+    const productCount = productsListContainer.querySelectorAll(".product-container").length;
+    const expectedDeliveryTimeContainer = document.getElementById("expected-delivery-time");
     if (productsListContainer && noImageContainer) {
-        if (productsListContainer.querySelectorAll(".product-container").length === 0) {
+        if (productCount === 0) {
             noImageContainer.classList.remove("d-none");
             noImageContainer.classList.add("d-flex");
             checkoutButton.classList.add("disabled");
+            cartClearButton.classList.add("disabled");
+            expectedDeliveryTimeContainer.classList.add("d-none");
         } else {
             noImageContainer.classList.toggle("d-flex");
             noImageContainer.classList.add("d-none");
             checkoutButton.classList.remove("disabled");
+            cartClearButton.classList.remove("disabled");
+            expectedDeliveryTimeContainer.classList.remove("d-none");
         }
     }
 }
 document.addEventListener("DOMContentLoaded", function () {
-    checkCardBodyContent();
+    emptyCartHandler();
 });
