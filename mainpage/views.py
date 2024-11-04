31e377d881c32f22e2e2ff284d5151da9507399a
Leviinson <votosi197@gmail.com>
2024-11-04 18:11:24 +0100

    feat(views): fetching of recommended products list from DB optimized, product carts fetching added

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,15 +1,15 @@
 from django.contrib.sites.shortcuts import get_current_site
-from django.core.paginator import Paginator
+from django.db.models import OuterRef, Subquery
 from django.utils.translation import gettext_lazy as _
 from django.views.generic.base import TemplateView
 
-from catalogue.models import Bouquet
+from cart.cart import BouquetCart, ProductCart
+from catalogue.models import Bouquet, BouquetImage
 from core.services.mixins.views import CommonContextMixin
 
 from .models import MainPageSliderImages
 
 
-# Create your views here.
 class MainPageView(CommonContextMixin, TemplateView):
     template_name = "mainpage/index.html"
     http_method_names = ["get"]
@@ -22,11 +22,31 @@ class MainPageView(CommonContextMixin, TemplateView):
         context["slider_images"] = MainPageSliderImages.objects.filter(
             is_active=True
         ).all()
-        bouquets = Bouquet.objects.all()
-
-        paginator = Paginator(bouquets, 4)
-        product_chunks = [
-            paginator.page(i).object_list for i in range(1, paginator.num_pages + 1)
-        ]
-        context["recommended_products"] = product_chunks
+        first_image_subquery = BouquetImage.objects.filter(
+            **{
+                "bouquet": OuterRef("pk"),
+            }
+        ).order_by("id")[:1]
+        bouquets = (
+            Bouquet.objects.select_related("subcategory", "subcategory__category")
+            .only(
+                "name",
+                "price",
+                "slug",
+                "subcategory__slug",
+                "subcategory__category__slug",
+            )
+            .annotate(
+                first_image_uri=Subquery(first_image_subquery.values("image")[:1]),
+            )
+            .order_by("amount_of_orders", "amount_of_savings")
+            .all()
+        )
+        context["recommended_products"] = bouquets
+        context["products_cart"] = ProductCart(
+            self.request.session, session_key="products_cart"
+        )
+        context["bouquets_cart"] = BouquetCart(
+            self.request.session, session_key="bouquets_cart"
+        )
         return context
