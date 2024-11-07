385b7317ee8d9e4ce30467a6ccf204a237950821
Leviinson <votosi197@gmail.com>
2024-11-07 11:32:57 +0100

    feat(models): IndividualOrder model created

--- mainpage/models.py
+++ mainpage/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class MainPageSliderImages(models.Model):
@@ -8,3 +11,22 @@ class MainPageSliderImages(models.Model):
     class Meta:
         verbose_name = "Фото слайдера главной страницы"
         verbose_name_plural = "Фотографии слайдера главной страницы"
+
+
+class IndividualOrder(models.Model):
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="individual_orders",
+        verbose_name="Связанный аккаунт",
+        null=True,
+        blank=False,
+    )
+    first_name = models.CharField(max_length=40)
+    phonenumber = PhoneNumberField(
+        max_length=15,
+        verbose_name=_("Номер телефона"),
+        null=True,
+        blank=True,
+        unique=True,
+    )
