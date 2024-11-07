91335fc0abac19b25468a9da55fbba4852d95db2
Leviinson <votosi197@gmail.com>
2024-11-07 11:25:28 +0100

    feat(forms): field BootstrapSplitPhoneNumberField moved to "core" folder

--- accounts/forms/user_form.py
+++ accounts/forms/user_form.py
@@ -1,35 +1,8 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.forms.widgets import Select
 from django.utils.translation import gettext_lazy as _
-from phonenumber_field.formfields import PrefixChoiceField, SplitPhoneNumberField
 
-
-class BootstrapSplitPhoneNumberField(SplitPhoneNumberField):
-    def prefix_field(self):
-        field = PrefixChoiceField(
-            widget=Select(
-                attrs={
-                    "class": "form-select contry-code-select",
-                    "autocomplete": "tel-country-code",
-                    "style": "width: 40%;",
-                }
-            )
-        )
-        return field
-
-    def number_field(self):
-        return forms.CharField(
-            widget=forms.TextInput(
-                attrs={
-                    "type": "tel",
-                    "class": "form-control ms-2",
-                    "autocomplete": "tel-national",
-                    "area-describedby": "phoneNumberHelp",
-                    "maxlength": 10,
-                }
-            )
-        )
+from core.forms import BootstrapSplitPhoneNumberField
 
 
 class UserForm(forms.ModelForm):
---
+++ core/forms/phone_number.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.forms.widgets import Select
+from django.utils.translation import gettext_lazy as _
+from phonenumber_field.formfields import SplitPhoneNumberField
+
+from .fields import CountryCodePrefixChoiceField
+
+
+class BootstrapSplitPhoneNumberField(SplitPhoneNumberField):
+    def prefix_field(self):
+        field = CountryCodePrefixChoiceField(
+            widget=Select(
+                attrs={
+                    "class": "form-select contry-code-select text-start",
+                    "autocomplete": "tel-country-code",
+                    "style": "width: 40%;",
+                }
+            )
+        )
+        return field
+
+    def number_field(self):
+        return forms.CharField(
+            widget=forms.TextInput(
+                attrs={
+                    "type": "tel",
+                    "class": "form-control ms-2 text-start",
+                    "autocomplete": "tel-national",
+                    "area-describedby": "phoneNumberHelp",
+                    "maxlength": 10,
+                }
+            )
+        )
