350f825d4138029314574e803a9f11488e802dc3
Leviinson <votosi197@gmail.com>
2024-11-07 11:31:06 +0100

    feat(forms): BootstrapSplitPhoneNumberField moved to "core/forms"

---
+++ core/forms/fields.py
@@ -0,0 +1,18 @@
+from django.forms import ChoiceField
+from django.utils.translation import gettext_lazy as _
+from phonenumber_field.formfields import REGION_CODE_TO_COUNTRY_CODE
+
+
+def get_prefix_choices():
+    choices = [("", "---------")]
+    for region_code, country_code in REGION_CODE_TO_COUNTRY_CODE.items():
+        choices.append((region_code, f"{region_code} +{country_code}"))
+    return choices
+
+
+class CountryCodePrefixChoiceField(ChoiceField):
+    def __init__(self, *, choices=None, **kwargs):
+        if choices is None:
+            choices = get_prefix_choices()
+            choices.sort(key=lambda item: item[1])
+        super().__init__(choices=choices, **kwargs)
