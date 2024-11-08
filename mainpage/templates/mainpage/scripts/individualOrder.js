11beb5f22adc2665ad88d0f5210e78c4e5f8a741
Leviinson <votosi197@gmail.com>
2024-11-08 14:22:51 +0100

    feat(scripts): individualForm AJAX sending created

---
+++ mainpage/templates/mainpage/scripts/individualOrder.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const individualOrderForm = document.getElementById('individual-order-form');
+
+    async function sendAjax(form) {
+        try {
+            const response = await fetch(form.action, {
+                method: 'POST',
+                body: new FormData(form)
+            });
+
+            const data = await response.json();
+
+            if (data.status === "success") {
+                showToast(data.message, "success");
+                disableForm(form);
+            } else {
+                let errors = JSON.parse(data.errors);
+                let errorsText = Object.entries(errors)
+                    .map(([field, errors]) => {
+                        return errors.map(error => `${error.message}`).join('\n');
+                    })
+                    .join('\n');
+
+                showToast(`${data.message}\n${errorsText}`, "danger");
+            }
+        } catch (error) {
+            console.log(error);
+            showToast(gettext("Произошла ошибка. Повторите попытку позже."), "danger");
+        }
+    }
+
+    individualOrderForm.addEventListener('submit', function (e) {
+            e.preventDefault();
+            sendAjax(individualOrderForm);
+        });
+    });
