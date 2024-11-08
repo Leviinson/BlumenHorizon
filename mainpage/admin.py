9e266b29842bd16c130b5701a669cf7a930ee237
Leviinson <votosi197@gmail.com>
2024-11-08 14:20:38 +0100

    feat(admin): fields listing updated relating to the models fields

--- mainpage/admin.py
+++ mainpage/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import MainPageSliderImages
+from .models import IndividualOrder, MainPageSliderImages, SeoBlock
 
 
 class SliderImagesInline(admin.TabularInline):
@@ -13,3 +13,16 @@ class SliderImagesInline(admin.TabularInline):
 @admin.register(MainPageSliderImages)
 class MainPageSliderAdmin(admin.ModelAdmin):
     fields = ["image", "is_active"]
+    list_display = ["image", "is_active"]
+
+
+@admin.register(SeoBlock)
+class SeoBlockAdmin(admin.ModelAdmin):
+    fields = ["image", "alt"]
+    list_display = ["image", "alt"]
+
+
+@admin.register(IndividualOrder)
+class IndividualOrderAdmin(admin.ModelAdmin):
+    fields = ["first_name", "contact_method", "recall_me", "user"]
+    list_display = ["user", "first_name", "contact_method", "recall_me"]
