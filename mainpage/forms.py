16b04afdef5b256266df1fe791ea043759c74945
Leviinson <votosi197@gmail.com>
2024-11-08 14:20:53 +0100

    feat(forms): IndividualOrderForms fields updated

--- mainpage/forms.py
+++ mainpage/forms.py
@@ -2,21 +2,29 @@ from django import forms
 from django.utils.translation import gettext_lazy as _
 
 from accounts.models import User
-from core.forms import BootstrapSplitPhoneNumberField
 from mainpage.models import IndividualOrder
 
 
 class IndividualOrderForm(forms.ModelForm):
-    phonenumber = BootstrapSplitPhoneNumberField(label=_("Номер телефона"))
 
     class Meta:
         model = IndividualOrder
-        fields = ("first_name", "phonenumber")
+        fields = ("first_name", "contact_method", "recall_me")
+        widgets = {
+            "contact_method": forms.Textarea(
+                attrs={
+                    "style": "height: 60px; width: 100%",
+                    "resize": "vertical",
+                    "class": "form-control",
+                    "id": "contact-method-input",
+                    "autocomplete": "tel",
+                }
+            ),
+        }
 
     def save(self, commit=False, user: User = None):
         order: IndividualOrder = super().save(commit=False)
-        if user.is_authenticated:
+        if user and user.is_authenticated:
             order.user = user
-        if commit:
-            order.save()
+        order.save(commit)
         return order
