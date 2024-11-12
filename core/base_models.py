145d21f8b5982e501118fc55b12df6b336ac2afd
Leviinson <votosi197@gmail.com>
2024-11-13 00:08:06 +0100

    feat(models): param "editable=false" added for created_at and updated_at attributes

--- core/base_models.py
+++ core/base_models.py
@@ -4,9 +4,15 @@ from django.utils.translation import gettext_lazy as _
 
 class TimeStampAdbstractModel(models.Model):
     created_at = models.DateTimeField(
-        auto_now_add=True, verbose_name=_("Дата создания")
+        auto_now_add=True,
+        verbose_name=_("Дата создания"),
+        editable=False,
     )
-    updated_at = models.DateTimeField(auto_now=True, verbose_name=_("Дата обновления"))
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_("Дата обновления"),
+        editable=False,
+    )
 
     class Meta:
         abstract = True
