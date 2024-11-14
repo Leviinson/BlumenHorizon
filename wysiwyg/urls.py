b9928b58aa75dc70d79bf658125a5915875b9a9f
Leviinson <votosi197@gmail.com>
2024-11-14 13:56:47 +0100

    refactoring(urls): DRY used

--- wysiwyg/urls.py
+++ wysiwyg/urls.py
@@ -1,18 +1,16 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import user_passes_test
 from django.urls import include, path
 
 from .views import image_upload
 
-
-def superuser_required(function):
-    return user_passes_test(lambda user: user.is_superuser)(function)
-
-
 app_name = "tiny-mce"
 
 urlpatterns = [
-    path("tinymce/", superuser_required(include("tinymce.urls"))),
+    path("tinymce/", staff_member_required(include("tinymce.urls"))),
     path(
-        "tinymce-image-upload/", superuser_required(image_upload), name="image_upload"
+        "tinymce-image-upload/",
+        staff_member_required(image_upload),
+        name="image_upload",
     ),
 ]
