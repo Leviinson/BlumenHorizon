e0a4682526687cc97873ef4672a587d5ded9f743
Leviinson <103764172+Leviinson@users.noreply.github.com>
2024-11-22 15:07:53 +0100

    feat(wsgi): gunicorn config created

---
+++ gunicorn.conf.py
@@ -0,0 +1,17 @@
+import multiprocessing
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+bind = os.getenv("GUNICORN_DOMAIN")
+workers = multiprocessing.cpu_count() * 2 + 1
+timeout = 120
+cache = None
+reload = False
+wsgi_app = "config.wsgi:application"
+
+accesslog = "logs/gunicorn/gunicorn_access.log"
+errorlog = "logs/gunicorn/gunicorn_error.log"
+loglevel = "debug"
