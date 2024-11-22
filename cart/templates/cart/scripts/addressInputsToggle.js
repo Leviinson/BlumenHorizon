45bc96a86daa13d536c5137036c51cd9beee04e6
Leviinson <103764172+Leviinson@users.noreply.github.com>
2024-11-22 03:15:18 +0100

    feat(scripts): "required" input attribute toggling added

--- cart/templates/cart/scripts/addressInputsToggle.js
+++ cart/templates/cart/scripts/addressInputsToggle.js
@@ -1,4 +1,4 @@
-document.getElementById('confirm-address').addEventListener('change', function() {
+function addressInputsToggle() {
     const addressFields = [
         'country-container', 'city-container', 'postal-code-container',
         'street-container', 'building-number-container', 'apartment-container',
@@ -11,10 +11,14 @@ document.getElementById('confirm-address').addEventListener('change', function()
         if (this.checked) {
             container.style.display = 'none';
             ['country-container', 'city-container'].includes(containerId) ? '' : input.value = '';
+            input.removeAttribute('required');
             container.querySelector('label').style.display = 'none';
         } else {
             container.style.display = 'block';
+            input.setAttribute('required', '')
             container.querySelector('label').style.display = 'block';
         }
     });
-});
+}
+
+document.getElementById('confirm-address').addEventListener('change', addressInputsToggle);
