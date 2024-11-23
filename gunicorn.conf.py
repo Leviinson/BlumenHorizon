3e540047078bb88b36f38b9784909350e2033f9e
Leviinson <103764172+Leviinson@users.noreply.github.com>
2024-11-23 22:01:12 +0100

    fix(gunicorn): wsgi_app path fixed

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -10,7 +10,7 @@ workers = multiprocessing.cpu_count() * 2 + 1
 timeout = 120
 cache = None
 reload = False
-wsgi_app = "config.wsgi:application"
+wsgi_app = "core.wsgi:application"
 
 accesslog = "logs/gunicorn/gunicorn_access.log"
 errorlog = "logs/gunicorn/gunicorn_error.log"
