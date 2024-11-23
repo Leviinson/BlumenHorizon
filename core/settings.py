223a7f05cf2cbc3e6cf00c93ebb3d5637b1f5640
Leviinson <103764172+Leviinson@users.noreply.github.com>
2024-11-23 14:16:34 +0100

    fix(settings): LocaleMiddleware position in list fixed: before CommonMiddleware, after SessionMiddleware

--- core/settings.py
+++ core/settings.py
@@ -141,11 +141,11 @@ SERVER_EMAIL = os.getenv("SERVER_EMAIL")
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
