d984a1e2ae4804b78f05ecd0c076eed7389f0a13
Leviinson <103764172+Leviinson@users.noreply.github.com>
2024-11-23 20:46:46 +0100

    fix(scripts): individual order form action attribute getting updated

--- mainpage/templates/mainpage/scripts/individualOrder.js
+++ mainpage/templates/mainpage/scripts/individualOrder.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
         const individualOrderForm = document.getElementById('individual-order-form');
+        const action = individualOrderForm.dataset.action;
 
         async function sendAjax(form) {
             try {
