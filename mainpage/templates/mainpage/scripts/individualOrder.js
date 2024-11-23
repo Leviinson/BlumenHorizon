4fb1d1ff002f595388b82c6898644aa1658c493c
Leviinson <103764172+Leviinson@users.noreply.github.com>
2024-11-23 19:41:01 +0100

    fix(scripts): https://github.com/facebook/react/issues/13369

--- mainpage/templates/mainpage/scripts/individualOrder.js
+++ mainpage/templates/mainpage/scripts/individualOrder.js
@@ -1,36 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const individualOrderForm = document.getElementById('individual-order-form');
+        const individualOrderForm = document.getElementById('individual-order-form');
 
-    async function sendAjax(form) {
-        try {
-            const response = await fetch(form.action, {
-                method: 'POST',
-                body: new FormData(form)
-            });
+        async function sendAjax(form) {
+            try {
+                const response = await fetch(form.action, {
+                    method: 'POST',
+                    body: new FormData(form)
+                });
 
-            const data = await response.json();
+                const data = await response.json();
 
-            if (data.status === "success") {
-                showToast(data.detail, "success");
-                disableForm(form);
-            } else {
-                let errors = JSON.parse(data.errors);
-                let errorsText = Object.entries(errors)
-                    .map(([field, errors]) => {
-                        return errors.map(error => `${error.message}`).join('\n');
-                    })
-                    .join('\n');
+                if (data.status === "success") {
+                    showToast(data.detail, "success");
+                    disableForm(form);
+                } else {
+                    let errors = JSON.parse(data.errors);
+                    let errorsText = Object.entries(errors)
+                        .map(([field, errors]) => {
+                            return errors.map(error => `${error.message}`).join('\n');
+                        })
+                        .join('\n');
 
-                showToast(`${data.detail}\n${errorsText}`, "danger");
+                    showToast(`${data.detail}\n${errorsText}`, "danger");
+                }
+            } catch (error) {
+                console.log(error);
+                showToast(gettext("Произошла ошибка. Повторите попытку позже."), "danger");
             }
-        } catch (error) {
-            console.log(error);
-            showToast(gettext("Произошла ошибка. Повторите попытку позже."), "danger");
         }
-    }
 
-    individualOrderForm.addEventListener('submit', function (e) {
-            e.preventDefault();
-            sendAjax(individualOrderForm);
-        });
-    }, {passive: false});
+        individualOrderForm.addEventListener(
+            'submit',
+            function (e) {
+                e.preventDefault();
+                sendAjax(individualOrderForm);
+            },
+            {passive: false}
+        );
+    }
+);
