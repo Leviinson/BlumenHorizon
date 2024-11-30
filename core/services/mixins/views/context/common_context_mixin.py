d4f387340c4083c68de6e8b641eba4ef1c2154e1
Leviinson <103764172+Leviinson@users.noreply.github.com>
2024-12-01 00:53:40 +0100

    feat(context): key country_code added to common context

--- core/services/mixins/views/context/common_context_mixin.py
+++ core/services/mixins/views/context/common_context_mixin.py
@@ -2,21 +2,25 @@ from django.conf import settings
 from django.contrib.sites.models import Site
 from django.contrib.sites.shortcuts import get_current_site
 
+from extended_contrib_models.models import ExtendedSite
+
 
 class CommonContextMixin:
     def get_context_data(self, *args, **kwargs):
         "Must be implemented and inherited by every view"
         context = super().get_context_data(*args, **kwargs)
         current_site = self.current_site = get_current_site(self.request)
+        site_extended: ExtendedSite = self.current_site.extended
         if not context.get("site_name"):
             context["site_name"] = current_site.name
             context["domain_name"] = current_site.domain
             context["company_email"] = settings.EMAIL_HOST_USER
-        context["currency_symbol"] = current_site.extended.currency_symbol
-        context["currency_code"] = current_site.extended.currency_code
-        context["country"] = current_site.extended.country
-        context["city"] = current_site.extended.city
-        context["socials_right_bottom"] = current_site.extended.socials.all()
+        context["currency_symbol"] = site_extended.extended.currency_symbol
+        context["currency_code"] = site_extended.extended.currency_code
+        context["country"] = site_extended.extended.country
+        context["country_code"] = site_extended.country_iso_3166_1_alpha_2
+        context["city"] = site_extended.extended.city
+        context["socials_right_bottom"] = site_extended.extended.socials.all()
         context["MEDIA_URL"] = settings.MEDIA_URL
-        context["alert"] = current_site.extended.header_alert_message
+        context["alert"] = site_extended.extended.header_alert_message
         return context
