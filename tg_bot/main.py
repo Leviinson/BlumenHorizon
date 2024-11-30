d96ffec9aebafb89d766b808f10606f32e8fd9b8
Leviinson <103764172+Leviinson@users.noreply.github.com>
2024-12-01 00:54:38 +0100

    feat(formatters): black

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -1,6 +1,6 @@
+import asyncio
 import logging
 
-import asyncio
 from django.conf import settings
 from telegram import Bot
 from telegram.error import TelegramError
@@ -16,6 +16,7 @@ async def send_message_to_telegram_async(chat_id, text):
         await bot.send_message(chat_id, text, parse_mode="Markdown")
     except TelegramError as e:
         logger.error(msg=f"Error while sending message: {e}")
+
 
 def send_message_to_telegram(chat_id, text):
     loop = asyncio.get_event_loop()
