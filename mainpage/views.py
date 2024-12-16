0e38e62a1ef4d3b30247a9af355a95d3811742fb
Leviinson <103764172+Leviinson@users.noreply.github.com>
2024-12-16 16:23:18 +0100

    fix(views): fetching of the condition file updating date fixed

--- mainpage/views.py
+++ mainpage/views.py
@@ -234,7 +234,7 @@ class ConditionsViewMixin(CommonContextMixin):
         context["meta_tags"] = page.meta_tags
         context["url"] = self.url
         context["title"] = self.title
-        context["updated_at"] = self.updated_at
+        context["updated_at"] = page.updated_at
         return context
 
 
