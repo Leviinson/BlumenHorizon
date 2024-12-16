9f76a473328f1eed4a6792cd013056d5fa134ca7
Leviinson <103764172+Leviinson@users.noreply.github.com>
2024-12-16 02:57:10 +0100

    refactoring(livesearch): decomposed on several function

--- livesearch/views.py
+++ livesearch/views.py
@@ -7,33 +7,45 @@ from django.http import HttpRequest, JsonResponse
 from catalogue.models import Bouquet, Product
 
 
-def live_search(request: HttpRequest) -> JsonResponse:
+def generate_cache_key(query: str) -> str:
+    query_hash = hashlib.md5(query.encode("utf-8")).hexdigest()
+    return f"search_results_{query_hash}"
+
+
+def perform_search(query: str) -> dict:
     data = {"results": {"products": defaultdict(list), "bouquets": defaultdict(list)}}
 
-    query = request.GET.get("q", "")
-    query_hash = hashlib.md5(query.encode("utf-8")).hexdigest()
-    cache_key = f"search_results_{query_hash}"
-    if query:
-        cached_data = cache.get(cache_key)
-        if cached_data:
-            return JsonResponse(cached_data)
-
-        for model, key in [(Product, "products"), (Bouquet, "bouquets")]:
-            results = (
-                model.objects.select_related("subcategory__category", "subcategory")
-                .only(
-                    "name",
-                    "subcategory__category__name",
-                    "subcategory__slug",
-                    "subcategory__category__slug",
-                    "slug",
-                )
-                .filter(name__icontains=query)[:10]
+    for model, key in [(Product, "products"), (Bouquet, "bouquets")]:
+        results = (
+            model.objects.select_related("subcategory__category", "subcategory")
+            .only(
+                "name",
+                "subcategory__category__name",
+                "subcategory__slug",
+                "subcategory__category__slug",
+                "slug",
             )
-            for item in results:
-                category_name = item.subcategory.category.name
-                data["results"][key][category_name].append(
-                    {"name": item.name, "url": item.get_relative_url()}
-                )
-        cache.set(cache_key, data, timeout=60 * 60)
+            .filter(name__icontains=query)[:10]
+        )
+        for item in results:
+            category_name = item.subcategory.category.name
+            data["results"][key][category_name].append(
+                {"name": item.name, "url": item.get_relative_url()}
+            )
+
+    return data
+
+
+def live_search(request: HttpRequest) -> JsonResponse:
+    query = request.GET.get("q", "")
+    if not query:
+        return JsonResponse({"results": {"products": {}, "bouquets": {}}})
+
+    cache_key = generate_cache_key(query)
+    cached_data = cache.get(cache_key)
+    if cached_data:
+        return JsonResponse(cached_data)
+
+    data = perform_search(query)
+    cache.set(cache_key, data, timeout=60 * 60)
     return JsonResponse(data)
