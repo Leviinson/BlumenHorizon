f99085663be0c6e2db21ffb7064fe1605ab99823
Leviinson <103764172+Leviinson@users.noreply.github.com>
2024-12-30 02:45:00 +0100

    feat(refactoring): добавлены описания по PEP-257

--- livesearch/views.py
+++ livesearch/views.py
@@ -8,11 +8,36 @@ from catalogue.models import Bouquet, Product
 
 
 def generate_cache_key(query: str) -> str:
+    """
+    Генерирует уникальный ключ для кеширования на основе строки поиска.
+
+    Используется хэширование MD5 для получения уникального идентификатора
+    для каждого поискового запроса, чтобы предотвратить конфликт кешированных данных.
+
+    Args:
+        query (str): Строка запроса для поиска.
+
+    Returns:
+        str: Уникальный ключ для кеширования результатов поиска.
+    """
     query_hash = hashlib.md5(query.encode("utf-8")).hexdigest()
     return f"search_results_{query_hash}"
 
 
 def perform_search(query: str) -> dict:
+    """
+    Выполняет поиск по товарам и букетам по строке запроса.
+
+    Выполняет фильтрацию товаров и букетов по совпадению с именем, используя
+    запрос из строки и возвращает данные о первых 10 результатах.
+
+    Args:
+        query (str): Строка запроса для поиска.
+
+    Returns:
+        dict: Результаты поиска в формате словаря, содержащего разделение на продукты и букеты
+              по категориям.
+    """
     data = {"results": {"products": defaultdict(list), "bouquets": defaultdict(list)}}
 
     for model, key in [(Product, "products"), (Bouquet, "bouquets")]:
@@ -37,6 +62,18 @@ def perform_search(query: str) -> dict:
 
 
 def live_search(request: HttpRequest) -> JsonResponse:
+    """
+    Обрабатывает запрос на поиск и возвращает результаты в формате JSON.
+
+    Если запрос уже кеширован, возвращает данные из кеша. Если нет, выполняет поиск и кеширует
+    результаты для дальнейших запросов.
+
+    Args:
+        request (HttpRequest): Объект запроса, содержащий строку поиска.
+
+    Returns:
+        JsonResponse: Ответ с результатами поиска в формате JSON.
+    """
     query = request.GET.get("q", "")
     if not query:
         return JsonResponse({"results": {"products": {}, "bouquets": {}}})
