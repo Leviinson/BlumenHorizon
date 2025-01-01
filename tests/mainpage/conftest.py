aee51869a0c9044b66180201d1cc1ab0d128e6ca
Leviinson <103764172+Leviinson@users.noreply.github.com>
2025-01-02 00:45:58 +0100

    feat(tests): тестирование социальных сотей добавлено

--- tests/mainpage/conftest.py
+++ tests/mainpage/conftest.py
@@ -1,25 +1,98 @@
-import pytest
+from typing import TypedDict
 
+import pytest
+from django.contrib.sites.models import Site
+
+from extended_contrib_models.models import ExtendedSite, Social
 from mainpage.models import MainPageModel, MainPageSeoBlock, MainPageSliderImages
 
 from .types import MainPageData
 
 
 @pytest.fixture
-def mainpage(db) -> MainPageData:
-    mainpage = MainPageModel.objects.create(
+def create_mainpage(db) -> MainPageModel:
+    """Создает основную модель главной страницы."""
+    return MainPageModel.objects.create(
         meta_tags="<title> | BlumenHorizon</title>",
         json_ld_description="Blumen Horizon интернет-магазин цветов и подарков в Берлине",
         description="Описание странички в самом низу.",
     )
 
-    mainpage_seo_block = MainPageSeoBlock.objects.create(
+
+@pytest.fixture
+def create_mainpage_seo_block(db) -> MainPageSeoBlock:
+    """Создает SEO-блок для главной страницы."""
+    return MainPageSeoBlock.objects.create(
         image="tests/default_image_for_tests.jpg", image_alt="Описание сео-блока."
     )
-    mainpage_slider_images = MainPageSliderImages.objects.create(
+
+
+@pytest.fixture
+def create_mainpage_slider_image(db) -> MainPageSliderImages:
+    """Создает изображение для слайдера на главной странице."""
+    return MainPageSliderImages.objects.create(
         image="tests/default_image_for_tests.jpg",
         image_alt="Описание изображения для слайдера на главной странице.",
         is_active=True,
     )
-    return mainpage, mainpage_seo_block, mainpage_slider_images
 
+
+class ExpectedSocial(TypedDict):
+    absolute_url: str
+    outline_hex_code: str
+    background_hex_code: str
+    icon_hex_code: str
+    bootstrap_icon: str
+    extended_site: ExtendedSite
+
+
+EXPECTED_SOCIALS = [
+    ExpectedSocial(
+        absolute_url="https://t.me/levinsxn/",
+        outline_hex_code="#000000",
+        background_hex_code="#111111",
+        icon_hex_code="#222222",
+        bootstrap_icon="telegram",
+    ),
+    ExpectedSocial(
+        absolute_url="https://instagram.com/",
+        outline_hex_code="#333333",
+        background_hex_code="#444444",
+        icon_hex_code="#555555",
+        bootstrap_icon="instagram",
+    ),
+    ExpectedSocial(
+        absolute_url="https://facebook.com/",
+        outline_hex_code="#666666",
+        background_hex_code="#777777",
+        icon_hex_code="#888888",
+        bootstrap_icon="facebook",
+    ),
+]
+
+
+@pytest.fixture
+def create_socials(db, site: Site) -> list[Social]:
+    """Создает объекты социальных сетей для связанного сайта."""
+    extended_site = ExtendedSite.objects.get(site=site)
+    socials_models_list = []
+    for social in EXPECTED_SOCIALS:
+        social["extended_site"] = extended_site
+        socials_models_list.append(Social(**social))
+    return Social.objects.bulk_create(socials_models_list)
+
+
+@pytest.fixture
+def mainpage(
+    create_mainpage,
+    create_mainpage_seo_block,
+    create_mainpage_slider_image,
+    create_socials,
+) -> MainPageData:
+    """Объединяет все данные главной страницы в одну фикстуру."""
+    return (
+        create_mainpage,
+        create_mainpage_seo_block,
+        create_mainpage_slider_image,
+        create_socials,
+    )
--- tests/mainpage/test_mainpage.py
+++ tests/mainpage/test_mainpage.py
@@ -1,8 +1,14 @@
+from django.conf import settings
 from django.contrib.sites.models import Site
 from django.template import Context
 from django.test import Client
+from django.db.models.query import QuerySet
+
+from extended_contrib_models.models import ExtendedSite, Social
+
 
 from ..types import UrlsDataclass
+from .conftest import EXPECTED_SOCIALS
 
 
 class TestMainPage:
@@ -16,6 +22,7 @@ class TestMainPage:
             context=response.context,
             site_domain=site.domain,
             site_name=site.name,
+            site_extended=site.extended,
         )
 
     def _assert_mainpage_context(
@@ -23,6 +30,7 @@ class TestMainPage:
         context: Context,
         site_domain: str,
         site_name: str,
+        site_extended: ExtendedSite,
     ) -> None:
         assert context["domain_name"] == site_domain
         assert context["site_name"] == site_name
@@ -30,3 +38,14 @@ class TestMainPage:
             context["json_ld_description"]
             == "Blumen Horizon интернет-магазин цветов и подарков в Берлине"
         )
+        assert context["company_email"] == settings.EMAIL_HOST_USER
+
+        assert len(context["socials_right_bottom"]) == 3
+        socials: QuerySet[Social] = context["socials_right_bottom"]
+        for social, expected_social in zip(socials, EXPECTED_SOCIALS):
+            assert social.absolute_url == expected_social["absolute_url"]
+            assert social.background_hex_code == expected_social["background_hex_code"]
+            assert social.bootstrap_icon == expected_social["bootstrap_icon"]
+            assert social.extended_site == site_extended
+            assert social.icon_hex_code == expected_social["icon_hex_code"]
+            assert social.outline_hex_code == expected_social["outline_hex_code"]
