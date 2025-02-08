eadf9c53e445dc7aaafded553b40cc43569eebbd
Leviinson <103764172+Leviinson@users.noreply.github.com>
2025-02-09 00:07:12 +0100

    feat(caching): использование нужного профиля CACHES добавлено

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -14,7 +14,9 @@ async def send_message_to_telegram_async(chat_id, text):
         await bot.send_message(chat_id, text, parse_mode="Markdown")
     except Exception as e:
         logger = logging.getLogger("telegramBot")
-        logger.error(f"Error sending message to chat_id {chat_id}: {e}", stack_info=True)
+        logger.error(
+            f"Error sending message to chat_id {chat_id}: {e}", stack_info=True
+        )
 
 
 async def send_messages_to_multiple_chat_ids(chat_ids, text):
@@ -39,8 +41,12 @@ def send_message_to_telegram(text: str):
             asyncio.set_event_loop(loop)
 
         if loop.is_running():
-            asyncio.ensure_future(send_messages_to_multiple_chat_ids(chat_ids_list, text))
+            asyncio.ensure_future(
+                send_messages_to_multiple_chat_ids(chat_ids_list, text)
+            )
         else:
-            loop.run_until_complete(send_messages_to_multiple_chat_ids(chat_ids_list, text))
+            loop.run_until_complete(
+                send_messages_to_multiple_chat_ids(chat_ids_list, text)
+            )
     except Exception as e:
         logger.error(f"Error in send_message_to_telegram: {e}")
